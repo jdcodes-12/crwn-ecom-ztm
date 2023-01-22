6dcdcee75a1e6a6a5577e0cd905c87b38e21a513
jdcodes-12 <johnnydicksoncode@gmail.com>
2023-01-21 18:03:55 -0600

    Create Button component

---
+++ src/components/buttons/button.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './button.styles.scss';
+
+const BUTTON_TYPE = {
+  google: 'google-sign-in',
+  inverted: 'inverted',
+};
+
+const Button = ({ children, buttonType, buttonProps}) => {
+  return (
+    <button 
+      className={`button-container ${BUTTON_TYPE[buttonType]}`}
+      {...buttonProps}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
