e04dcfef4fb76e06b89715268b3c68f044df24f8
jdcodes-12 <johnnydicksoncode@gmail.com>
2023-01-24 11:27:23 -0600

    Add styled-components to CategoriesMenu

--- src/components/category/categories-menu/categories-menu.component.jsx
+++ src/components/category/categories-menu/categories-menu.component.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import DirectoryItem from '../../directory-item/directory-item.component';
-import './categories-menu.styles.scss';
 import categories from '../../../data/categories.json';
-
+import { CategoriesMenuContainer } from './categories-menu.styles';
 
 const CategoriesMenu = () => {
  return (
-    <section className="categories-container">
+    <CategoriesMenuContainer as='section' className="categories-container">
       {categories.map(category => (
         <DirectoryItem key={category.id} category={category}/>
       ))}
-    </section>
+    </CategoriesMenuContainer>
  );
 }
 
