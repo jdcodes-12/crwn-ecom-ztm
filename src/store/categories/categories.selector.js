66a3d1874d5cc8aae7668cac8d88be67c0fbd0fb
jdcodes-12 <johnnydicksoncode@gmail.com>
2023-01-25 13:21:33 -0600

    Convert categories.selector to use reslect
    Added reselect for memoization of state objects. This is to help alleviate the unncessary rerendering in the application

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,6 +1,17 @@
-export const selectCategoriesMap = (state) => state.categories.categories
-  .reduce((accumulator, category) => {
+import { createSelector } from 'reselect';
+
+export const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categorySlice) => categorySlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => categories.reduce((accumulator, category) => {
     const { items, title } = category;
     accumulator[title.toLowerCase()] = items;
     return accumulator;
-  }, {});
+  }, {})
+);
