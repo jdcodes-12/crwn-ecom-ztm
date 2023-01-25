bdb8d2adef05e240fdac49be364eb316e2e41cb4
jdcodes-12 <johnnydicksoncode@gmail.com>
2023-01-25 17:29:11 -0600

    Setup composer to work with ReduxDevTools chrome extension

--- src/store/store.redux.js
+++ src/store/store.redux.js
@@ -18,11 +18,19 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// Define the middlewares that hit before dispatching
-const middlewares = [logger]
+// Define the middlewares that hit before dispatching & only apply in development
+const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+
+// Swaps between native compose from redux or using redux dev-tools
+const composer = 
+  (    process.env.NODE_ENV !== 'production' 
+    && window 
+    && window.__REDUX_DEVTOOLS_EXTENSION_CHROME__ 
+  )
+  || compose
 
 // Currying to create the ehancer chain
-const composedEnhancers = compose(applyMiddleware(...middlewares));
+const composedEnhancers = composer(applyMiddleware(...middlewares));
 
 // Hook up store with middlewares
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
